26335c14d8329e54966f38a32dd1831d1219ea4c
Carlos Francisco <86804652+calitoozzy@users.noreply.github.com>
2022-04-20 16:28:10 -0300

    TP 2 terminado

---
+++ Unidad 2/js/code.js
@@ -0,0 +1,24 @@
+
+var cantidadDeVentanasEnMiCasa = 10;
+console.log ("ventanas en mi casa " + cantidadDeVentanasEnMiCasa);
+
+var soyHumano = true;
+console.log (soyHumano);
+
+var  miGustoDePizzaPreferido = "roquefort";
+console.log (miGustoDePizzaPreferido);
+
+var muchosDatos = false;
+console.log (muchosDatos);
+
+muchosDatos = 0;
+console.log (muchosDatos);
+
+muchosDatos = "hola";
+console.log (muchosDatos);
+
+muchosDatos = "minions";
+console.log (muchosDatos);
+
+
+
